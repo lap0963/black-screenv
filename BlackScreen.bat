8a738643120587474940e6d2f431e14651265111
lap0963 <Bradstarlabs@gmail.com>
2025-11-04 15:53:17 -0800

    Update and rename Black-screen.exe to BlackScreen.bat

---
+++ BlackScreen.bat
@@ -0,0 +1,24 @@
+@echo off
+title haha dad
+color 0f
+mode 1000
+
+:: Clear screen and fill with black
+cls
+:: Hide cursor
+echo.
+
+:: Fullscreen black screen simulation
+:loop
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo             
+echo.
+echo              
+:: Wait for user input
+set /p key= 
+if "%key%"=="" goto loop
